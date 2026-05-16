7a8845e24accab6521169ab65e034a4edea29060
Sutriaji <sutriaji@citis.kompasgramedia.com>
2026-05-16 14:29:58 +0700

    Fix Maahir Deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
   experimental: {
     serverActions: {
       bodySizeLimit: '60mb', // 3 audio × ~15 menit (opus ~64kbps ≈ 7 MB/audio)
